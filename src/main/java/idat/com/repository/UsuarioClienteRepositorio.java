91129172e606b925472e51352a7e5c93da99bc73
Ana Gabriela <avilchezsullon8@gmail.com>
2022-07-21 22:20:04 -0500

    update

--- src/main/java/idat/com/repository/UsuarioClienteRepositorio.java
+++ src/main/java/idat/com/repository/UsuarioClienteRepositorio.java
@@ -7,5 +7,5 @@ import idat.com.model.UsuarioCliente;
 
 @Repository
 public interface UsuarioClienteRepositorio extends JpaRepository<UsuarioCliente, Integer> {
-
+	UsuarioCliente findByUsuario(String usuario);
 }
