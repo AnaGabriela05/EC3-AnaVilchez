9f5a2f9e53f675522f8b4bb25c0cd363cc7883f7
Ana Gabriela <avilchezsullon8@gmail.com>
2022-07-21 22:26:36 -0500

    update

--- src/main/java/idat/com/seguridad/SecurityConfig.java
+++ src/main/java/idat/com/seguridad/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers("/usuario/**").permitAll()
+			.antMatchers("/usuariocliente/**").permitAll()
 			.anyRequest()
 			.authenticated()
 			.and()
