fc41d7dff8a681fdb45fe2b88b1c75d16fd7193c
Ana Gabriela <avilchezsullon8@gmail.com>
2022-07-21 22:53:49 -0500

    update

---
+++ src/main/resources/Data.sql
@@ -0,0 +1,3 @@
+insert into usuario_cliente(usuario,password,rol,id_cliente) values ('ANA1720','1234','ROLE_USER',1)
+insert into usuario_cliente(usuario,password,rol,id_cliente)  values ('user23','4567','ROLE_USER',2)
+insert into usuario_cliente(usuario,password,rol,id_cliente)  values ('user45','8907','ROLE_USER',3)
